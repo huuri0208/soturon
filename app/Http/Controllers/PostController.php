c7f2d4400c2fdae07e03a6bee73cd4d5258437d6
huuri0208 <shokoravn@icloud.com>
2022-12-09 13:30:09 +0900

    タグ制作

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Requests\PostRequest;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -14,15 +15,22 @@ class PostController extends Controller
        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
     }
     
-    public function create()
+    public function create(Tag $tag)
 {
-    return view('posts/create');
+    return view('posts/create')->with(['tags' => $tag->get()]);
 }
 
-public function store(Post $post, PostRequest $request)
+
+public function store(Request $request, Post $post)
 {
-    $input = $request['post'];
-    $post->fill($input)->save();
+    $input_post = $request['post'];
+    $input_tags = $request->tags_array;  
+    
+   
+    $post->fill($input_post)->save();
+    
+ 
+    $post->tags()->attach($input_tags); 
     return redirect('/');
 }
 }
